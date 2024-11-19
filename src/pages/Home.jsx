eba515599b98da45c04f511023f6e04b7890ed05
DaveBers <cristopherspqr@gmail.com>
2024-11-18 21:16:03 -0300

    ajustes cores botões

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,10 +19,10 @@ const Home = () => {
   ]);
 
   const adicionarPeca = (i, retorno) => {
-    console.log('index',i);
-    console.log('retorno',retorno);
+    console.log('index', i);
+    console.log('retorno', retorno);
     const nComponentes = [...componentes];
-    
+
     nComponentes[i].peca = retorno;
 
     setTotal(total + retorno.valor);
@@ -41,16 +41,16 @@ const Home = () => {
 
   return (
     <Layout className="layout">
-      <Header style={{backgroundColor: 'transparent', textAlign: 'center', padding: '10px' }}>
-        <Title style={{ color: '#2d3277', margin: '20px', fontSize:'px', fontWeight: 'bold' }}>Monte seu PC!</Title>
+      <Header style={{ backgroundColor: 'transparent', textAlign: 'center', padding: '10px' }}>
+        <Title style={{ color: '#2d3277', margin: '20px', fontSize: 'px', fontWeight: 'bold' }}>Monte seu PC!</Title>
       </Header>
       <Content style={{ padding: '20px', maxWidth: '100%', margin: '20px' }}>
-        <Row justify="center" 
+        <Row justify="center"
           style={{ marginBottom: '20px' }}>
           <Col>
             <div className="price-box">
-              <Title level={2} 
-              style={{ textAlign: 'center', color: '#52C41A', padding: '10px', backgroundColor: '#002766', marginBottom: '10px', borderRadius: '10px' }}>
+              <Title level={2}
+                style={{ textAlign: 'center', color: '#52C41A', padding: '10px', backgroundColor: '#002766', marginBottom: '10px', borderRadius: '10px' }}>
                 R$ {total.toFixed(2)}
               </Title>
             </div>
@@ -58,12 +58,12 @@ const Home = () => {
         </Row>
         <List dataSource={componentes}
           renderItem={(item, index) => (
-            <List.Item style={{padding: '10px', backgroundColor: '#f7d02c', marginBottom: '10px', borderRadius: '10px', border: '1px solid black'}}>
-              <Row justify="start" 
+            <List.Item style={{ padding: '10px', backgroundColor: '#f7d02c', marginBottom: '10px', borderRadius: '10px', border: '1px solid black' }}>
+              <Row justify="start"
                 align="middle"
                 style={{ width: '100%' }}>
                 <Col span={3}>
-                  <Title level={4} 
+                  <Title level={4}
                     style={{ margin: 0 }}>
                     {item.nome}
                   </Title>
@@ -71,43 +71,64 @@ const Home = () => {
                 {!item?.peca?.descricao ?
                   <Col span={2}>
                     <ModalPecas peca={item.nome}
-                      recebePeca={(valor) => adicionarPeca(index,valor)}>
-<Button 
-  shape="circle"
-  icon={<PlusOutlined/>}
-  style={{
-    backgroundColor: '#ffffff', 
-    color: '#2d3277', 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    transition: 'transform 0.3s',
-  }}
-  onMouseEnter={(e) => {
-    e.currentTarget.style.transform = 'scale(1.2)';
-  }}
-  onMouseLeave={(e) => {
-    e.currentTarget.style.transform = 'scale(1)';
-  }}
-/>
+                      recebePeca={(valor) => adicionarPeca(index, valor)}>
+                      <Button
+                        shape="circle"
+                        icon={<PlusOutlined style={{ fontSize: '16px' }} />}
+                        style={{
+                          backgroundColor: '#ffffff',
+                          color: '#2d3277',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          transition: 'all 0.3s ease',
+                        }}
+                        onMouseEnter={(e) => {
+                          e.currentTarget.style.transform = 'scale(1.2)';
+                          e.currentTarget.style.backgroundColor = '#52c41a';
+                          e.currentTarget.style.color = '#ffffff';
+                        }}
+                        onMouseLeave={(e) => {
+                          e.currentTarget.style.transform = 'scale(1)';
+                          e.currentTarget.style.backgroundColor = '#ffffff';
+                          e.currentTarget.style.color = '#2d3277';
+                        }}
+                      />
                     </ModalPecas>
                   </Col>
-                : ''}
-                  {item?.peca?.descricao ? 
-                    <React.Fragment>
-                      <Col span={16}>
-                        <label style={{fontSize: 16}}>{item?.peca.descricao}</label>
-                      </Col>
-                      <Col span={3}>
-                        <label style={{color: '#52C41A', fontSize: 24}}>R$ {item?.peca?.valor.toFixed(2)}</label>
-                      </Col>
-                      <Col span={2}>
-                        <Button shape="circle"
-                          icon={<MinusOutlined />}
-                          style={{ backgroundColor: '#F5222D', color: '#000000' }}
-                          onClick={() => {removerPeca(index)}}/>
-                      </Col>
-                    </React.Fragment>
+                  : ''}
+                {item?.peca?.descricao ?
+                  <React.Fragment>
+                    <Col span={16}>
+                      <label style={{ fontSize: 16 }}>{item?.peca.descricao}</label>
+                    </Col>
+                    <Col span={3}>
+                      <label style={{ color: '#52C41A', fontSize: 24, fontWeight:'bold'}}>R$ {item?.peca?.valor.toFixed(2)}</label>
+                    </Col>
+                    <Col span={2}>
+                      <Button
+                        shape="circle"
+                        icon={<MinusOutlined style={{ fontSize: '16px' }} />}
+                        style={{
+                          backgroundColor: '#FFFFFF',
+                          color: '#000000',
+                          transition: 'all 0.3s ease',
+                          transform: 'scale(1)',
+                        }}
+                        onClick={() => { removerPeca(index); }}
+                        onMouseEnter={(e) => {
+                          e.currentTarget.style.backgroundColor = '#F5222D';
+                          e.currentTarget.style.color = '#FFFFFF';
+                          e.currentTarget.style.transform = 'scale(1.1)';
+                        }}
+                        onMouseLeave={(e) => {
+                          e.currentTarget.style.backgroundColor = '#FFFFFF';
+                          e.currentTarget.style.color = '#000000';
+                          e.currentTarget.style.transform = 'scale(1)';
+                        }}
+                      />
+                    </Col>
+                  </React.Fragment>
                   : ''}
               </Row>
             </List.Item>
